31340b55b95577f68e9333f533d254a8d83ff683
g0d <wuyen.mail@gmail.com>
2025-01-23 11:29:43 +0700

    fix type char c

--- main.c
+++ main.c
@@ -37,7 +37,7 @@ int main(int argc, char *argv[])
         {
             fatal("Can't open file");
         }
-        char c;
+        int c;
         while((c = getc(file)) != EOF)
         {
             putchar(c);
